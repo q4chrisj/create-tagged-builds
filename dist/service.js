7c33a9ed97cb6d547af5f2da7c3b39198676687c
Chris Jones <chrisj@q4inc.com>
2024-02-14 09:56:49 -0500

    working for the most basic use case
    use case: a repo that contains one git ref to update

---
+++ dist/service.js
@@ -0,0 +1,69 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.put = exports.post = exports.get = void 0;
+const axios_1 = __importDefault(require("axios"));
+const config_1 = require("./config");
+const teamCityToken = config_1.config.TeamCityToken;
+const headers = {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+    Authorization: `Bearer ${teamCityToken}`,
+};
+const get = async (path) => {
+    const requestUri = config_1.config.TeamCityUri.concat(path);
+    const result = await axios_1.default
+        .get(requestUri, {
+        headers: headers,
+    })
+        .then((r) => {
+        return r.data;
+    });
+    return result;
+};
+exports.get = get;
+const post = async (path, body, contentType) => {
+    if (contentType) {
+        headers["Content-Type"] = contentType;
+    }
+    const requestUri = config_1.config.TeamCityUri.concat(path);
+    const result = await axios_1.default
+        .post(requestUri, body, {
+        headers: headers,
+    })
+        .then((r) => {
+        return r.data;
+    })
+        .catch((err) => {
+        console.log("");
+        console.log(err.response.data);
+    });
+    return result;
+};
+exports.post = post;
+const put = async (path, body, contentType) => {
+    let specialHeaders;
+    if (contentType) {
+        specialHeaders = {
+            ...headers,
+            "Content-Type": contentType,
+            Accept: contentType,
+        };
+    }
+    const requestUri = config_1.config.TeamCityUri.concat(path);
+    const result = await axios_1.default
+        .put(requestUri, body, {
+        headers: specialHeaders || headers,
+    })
+        .then((r) => {
+        return r.data;
+    })
+        .catch((err) => {
+        console.log("");
+        console.log(err.response.data);
+    });
+    return result;
+};
+exports.put = put;
--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import * as core from "@actions/core";
 import {
   createNewProject,
   getMostRecentProject,
-  getProject,
   triggerBuild,
   updateProjectParameters,
 } from "./index.helpers";
@@ -82,63 +81,16 @@ export const run = async (): Promise<void> => {
   await updateProjectParameters(createResult.id, paramUpdates);
 
   // trigger the builds
-  console.log(`\nTriggering builds for new project ${createResult.name}\n`);
+  console.log(`\nTriggering builds for new project ${createResult.name}`);
   const validBuildTypeNames = ["Public CI", "Admin CI", "Preview CI", "CI"];
-  // const newProject: Project | undefined = await getProject(createResult.id);
   for (const buildType of createResult.buildTypes.buildType) {
     if (validBuildTypeNames.includes(buildType.name)) {
-      console.log(`Triggering build for ${buildType.name}`);
+      console.log(` - Triggering build for ${buildType.name}`);
       await triggerBuild(buildType.id);
     }
   }
 
   return;
-  /*
-  const latestFoundationTag: string = "v2.115.0"; // this would normally come from a github api call
-  const latestGoServicetag: string = "v2.67.0"; // this would normally come from a github api call
-
-
-  // update new project dependency parameters
-  const paramUpdates: UpdateParameters = {
-    parameters: [
-      {
-        name: "system.GitDefaultBranch-Q4Manhattan",
-        value: `tags/${newProjectName}`,
-      },
-      {
-        name: "system.GitDefaultBranch-Q4Orion-Go",
-        value: `tags/${latestGoServicetag}`,
-      },
-      {
-        name: "system.GitDefaultBranch-Q4Foundation",
-        value: `tags/${latestFoundationTag}`,
-      },
-      {
-        name: "env.version",
-        value: `${newProjectName.substring(1)}`,
-      },
-    ],
-  };
-
-  await updateProjectParameters(createResult.id, paramUpdates);
-  //
-  // here we can loop through the new projects builds and trigger them
-  // if we decide to go this route we can also reset the build counter for each project
-  console.log(`\nTriggering builds for new project ${createResult.name}\n`);
-
-  const validBuildTypeNames = ["Public CI", "Admin CI", "Preview CI"];
-  // const projectId = "Q4Web_Develop";
-  const projectId = createResult.id;
-  const newProject: Project | undefined = await getProject(projectId);
-  for (const buildType of newProject!.buildTypes.buildType) {
-    if (validBuildTypeNames.includes(buildType.name)) {
-      console.log(`Triggering build for ${buildType.name}`);
-      await triggerBuild(buildType.id);
-    }
-  }
-
-  return;
-  */
 };
 
 run();
