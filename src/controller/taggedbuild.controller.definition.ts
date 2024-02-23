2fe8ee92aa6c6ce5edd538e8974725a88dc8e893
Chris Jones <chrisj@q4inc.com>
2024-02-23 09:23:11 -0500

    maps for the most frequently deployed projects

--- src/controller/taggedbuild.controller.definition.ts
+++ src/controller/taggedbuild.controller.definition.ts
@@ -1,4 +1,19 @@
+/*
+ * This set maps dependant github system variables in the TeamCity project.
+ * Most of the projects in TeamCity have dependencies on other git repos
+ * (system.GitDefaultBranch-Q4Foundation) for example. The key of this set
+ * matches everything after system.GitDefaultBranch to a valid github repository
+ * which is the value of each entry
+ */
 export const DependantRepos: Map<string, string> = new Map<string, string>();
-DependantRepos.set("Q4Orion-Go", "Q4Web-Q4Orion-Go");
 DependantRepos.set("Q4Foundation", "Q4Web-Q4Foundation");
 DependantRepos.set("Q4Analytics", "Q4Web-Q4Analytics");
+DependantRepos.set("Q4Proxy", "Q4Web-Q4Proxy");
+DependantRepos.set("Q4Setup", "Q4Web-Q4Setup");
+
+DependantRepos.set("Q4Orion-Go", "Q4Web-Q4Orion-Go");
+DependantRepos.set("Q4CustomerAccount", "Q4Web-Q4Orion-CustomerAccount");
+DependantRepos.set("Q4Orion-Cloud", "Q4Web-Q4Orion-Cloud");
+DependantRepos.set("Q4Orion-NServiceBus", "Q4Web-Q4Orion-NServiceBus");
+DependantRepos.set("Q4Orion-SerialService", "Q4Web-Q4Orion-SerialService");
+DependantRepos.set("Q4Orion-Air", "Q4Web-Q4Orion-Air");
